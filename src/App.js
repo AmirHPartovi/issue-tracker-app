40c864e556ba31205e681105fc9861264efa27d8
AmirHPartovi <a.partovi99@gmail.com>
2023-07-02 13:55:13 +0330

    reactquery setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 
+const fetchUser=(username)=>{
+  return fetch(`https://api.github.com/users/${username}`)
+  .then(res => res.json())
+}
+const GithubUser = (username) =>{
+  const userQuery =useQuery(
+    ["username"],
+    ()=> fetchUser(username)
+  )
+  const data = userQuery.data
+  if(userQuery.isLoading) return <p> Loading ... </p>
+  
+}
 
 function App() {
   return (
     <div>
-
+        <GithubUser username={'uidotdev'}/>
     </div>
   );
 }
